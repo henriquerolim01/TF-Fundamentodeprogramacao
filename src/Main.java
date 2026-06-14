153336ab7387cb8caa51581d6a2e4edd15c6a4fe
Henrique Rolim <108377987+henriquerolim01@users.noreply.github.com>
2026-06-14 20:41:55 +0000

    Adiciona menun principal do sistema

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,62 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("=== Sistema de Controle Financeiro ===");
+        Scanner entrada = new Scanner(System.in);
+        int opcao;
 
+        do {
+
+            System.out.println("\n=== SISTEMA DE CONTROLE FINANCEIRO ===");
+            System.out.println("1 - Cadastrar Usuário");
+            System.out.println("2 - Cadastrar Categoria");
+            System.out.println("3 - Cadastrar Movimentação");
+            System.out.println("4 - Listar Usuários");
+            System.out.println("5 - Listar Categorias");
+            System.out.println("6 - Listar Movimentações");
+            System.out.println("0 - Sair");
+
+            System.out.print("Escolha uma opção: ");
+            opcao = entrada.nextInt();
+
+            switch (opcao) {
+
+                case 1:
+                    System.out.println("Cadastro de Usuário");
+                    break;
+
+                case 2:
+                    System.out.println("Cadastro de Categoria");
+                    break;
+
+                case 3:
+                    System.out.println("Cadastro de Movimentação");
+                    break;
+
+                case 4:
+                    System.out.println("Listagem de Usuários");
+                    break;
+
+                case 5:
+                    System.out.println("Listagem de Categorias");
+                    break;
+
+                case 6:
+                    System.out.println("Listagem de Movimentações");
+                    break;
+
+                case 0:
+                    System.out.println("Encerrando sistema...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");
+            }
+
+        } while (opcao != 0);
+
+        entrada.close();
     }
-
 }
