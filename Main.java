f9df3b85a4361968ddceb9d0b31a2501396b197d
Henrique Rolim <108377987+henriquerolim01@users.noreply.github.com>
2026-06-16 21:54:56 +0000

    Corrige valores de saldo e limite na classe Conta

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String [] args){
     Usuario p1 = new Usuario("Guilherme", 23, 'M');
     System.out.println(p1.toString());
-    Conta c1 = new Conta (p1, true, 123, 10.000, 20.000, "Santander");
+    Conta c1 = new Conta (p1, true, 123, 10000.0, 20000.0, "Santander");
     System.out.println(c1.toString());
 
 }
